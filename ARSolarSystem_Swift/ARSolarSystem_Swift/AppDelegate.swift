7094a4059116ba53a9c1e7fc216042a4193d4468
MBXB <bixiangbei_bfj@163.com>
2017-11-07 13:41:48 +0800

    ARSolarSystem_Swift

---
+++ ARSolarSystem_Swift/ARSolarSystem_Swift/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  ARSolarSystem_Swift
+//
+//  Created by Oboe_b on 2017/11/7.
+//  Copyright © 2017年 MBXB-bifujian. All rights reserved.
+//人的一生唯有学习和锻炼不可辜负
+//原文地址http://www.2bjs.com
+//微博https://weibo.com/u/6342211709
+//技术交流q群150731459
+//微信搜索iOS编程实战
+//https://github.com/MBXB/ARSolarSystem_Swift
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+
+
---
+++ ARSolarSystem_Swift/ARSolarSystem_Swift/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ARSolarSystem_Swift
+//
+//  Created by Oboe_b on 2017/11/7.
+//  Copyright © 2017年 MBXB-bifujian. All rights reserved.
+//人的一生唯有学习和锻炼不可辜负
+//原文地址http://www.2bjs.com
+//微博https://weibo.com/u/6342211709
+//技术交流q群150731459
+//微信搜索iOS编程实战
+//https://github.com/MBXB/ARSolarSystem_Swift
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
